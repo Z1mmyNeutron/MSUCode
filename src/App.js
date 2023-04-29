a6346951781d12f66e3f00f01c178cd881e9ad7e
Chrissie Zimmer <czimmer12@aol.com>
2023-04-29 14:08:07 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
+import { useState } from "react";
+
 import './App.css';
 
+const defaultProjects = [{
+  name: "Hawkhack",
+  description: "This is a project that we worked on",
+  tags: ["fun", "exciting", "scary", "spooky"]
+},
+{
+  name: "Art",
+  description: "This project is artistic",
+  tags: ["artsy"]
+}, {
+  name: "Disney",
+  description: "This project is a third project about disney",
+  tags: ["shrek"]
+}];
+
+const projectMapper = (project) => {
+  const projectStyle = {
+    backgroundColor: "rgba(255,200,200,50)",
+    width: "50%",
+    marginLeft: "25%",
+    borderRadius: 10
+  }
+  return (<>
+    <br></br>
+    <div style={projectStyle}>
+      <p style={{ fontWeight: "bold", color:"red" }}>{project.name}</p>
+      <p>{project.description}</p>
+    </div>
+  </>)
+}
+
 function App() {
+  const [projects, setProjects] = useState([...defaultProjects]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Projects</p>
+      {projects.map(projectMapper)}
+
+      <div>
+        <input type="text" placeholder="Name" style={{ width: "80%" }}
+          onChange={(event) => { setName(event.target.value); }}/>
+        <br></br>
+        <br></br>
+        <input type="text" placeholder="Description" style={{ width: "80%" }}
+        onChange={(event) => { setDescription(event.target.value); }}/>
+        <br></br>
+        <br></br>
+        <button style={{ fontSize: 20 }} onClick={() => {
+          let _projects = [...projects];
+
+          _projects.push({
+            name: name,
+            description: description
+          })
+          setProjects(_projects)
+        }}>Add Project</button>
+      </div>
     </div>
   );
 }
